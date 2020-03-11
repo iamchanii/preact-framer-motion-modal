f8b3f7d850704bbb36c44c6862e82806e1975578
iamchanii <iamchanii.0310@gmail.com>
2020-03-11 19:48:14 +0900

    Add postcss-nested plugin

--- postcss.config.js
+++ postcss.config.js
@@ -6,6 +6,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 module.exports = {
   plugins: [
     require('tailwindcss'),
+    require('postcss-nested'),
     ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 };
